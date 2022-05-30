7e37789d4b62d5c1a54eecc4a84ca68326308bad
Jacob Lin <jacoblindev@gmail.com>
2022-05-31 01:08:06 +0800

    fix: favicon relative path

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,10 +6,10 @@ module.exports = {
     title: 'JLDev',
     description: 'This is Jacob Lin\'s personal blog site',
     head: [
-        ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
-        ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' }],
-        ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }],
-        ['link', { rel: 'manifest', href: '/site.webmanifest' }],
+        ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/jldev/apple-touch-icon.png' }],
+        ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/jldev/favicon-32x32.png' }],
+        ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/jldev/favicon-16x16.png' }],
+        ['link', { rel: 'manifest', href: '/jldev/site.webmanifest' }],
     ],
 
     theme: defaultTheme({
