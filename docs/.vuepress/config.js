90d8a6973dde20064a9c589e75ce2c0c1be81614
Jacob Lin <jacoblindev@gmail.com>
2022-06-20 22:27:57 +0800

    feat: i18n setup

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,9 +3,20 @@ const { googleAnalyticsPlugin } = require('@vuepress/plugin-google-analytics')
 
 module.exports = {
     base: '/jldev/',
-    lang: 'en-US',
-    title: 'JLDev',
-    description: 'This is Jacob Lin\'s personal blog site',
+
+    locales: {
+        "/": {
+            lang: 'en-US',
+            title: 'JLDev',
+            description: 'This is Jacob Lin\'s personal blog site',
+        },
+        "/zh/": {
+            lang: 'zh-TW',
+            title: 'JLDev',
+            description: 'ˊ這是Jacob Lin的個人部落格網站',
+        },
+    },
+
     head: [
         ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/jldev/apple-touch-icon.png' }],
         ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/jldev/favicon-32x32.png' }],
@@ -16,6 +27,16 @@ module.exports = {
     theme: defaultTheme({
         logo: '/JLDev-Logo@2x.png',
         repo: 'https://github.com/jacoblindev',
+
+        locales: {
+            '/': {
+                selectLanguageName: 'English',
+            },
+            '/zh/': {
+                selectLanguageName: '繁體中文',
+            },
+        },
+
         navbar: [
             {
                 text: 'Home',
---
+++ docs/zh/notes/sample.md
@@ -0,0 +1,12 @@
+---
+title: 範例頁面
+editLink: false
+prev:
+  text: Dev Notes
+  link: /notes/
+next:
+  text: Projects
+  link: /projects/
+---
+
+## 有趣的內容
